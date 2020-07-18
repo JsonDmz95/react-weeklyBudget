c09436132fd4d5387932c1606581cc49c6c0e2ad
Jeeson Domiguez <dominguez_js@outlook.com>
2020-07-18 02:55:10 -0600

    Updates

---
+++ precache-manifest.65186ee81c62bb6169481f67331a5263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "174d824c64c9f8dc527acd5f8b19a16b",
+    "url": "/react-weeklyBudget/index.html"
+  },
+  {
+    "revision": "cc7b90ceca759289e7d6",
+    "url": "/react-weeklyBudget/static/css/main.0d6e8273.chunk.css"
+  },
+  {
+    "revision": "b587687b4b9894c51650",
+    "url": "/react-weeklyBudget/static/js/2.dcb571a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weeklyBudget/static/js/2.dcb571a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc7b90ceca759289e7d6",
+    "url": "/react-weeklyBudget/static/js/main.2d4608fe.chunk.js"
+  },
+  {
+    "revision": "a5d8fb5147cb8efe4bf7",
+    "url": "/react-weeklyBudget/static/js/runtime-main.1f38d51c.js"
+  }
+]);
